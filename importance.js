0377774f0da43fb1ffd5ab8c354b37d0690d91b2
Daiwik07 <130734114+Daiwik07@users.noreply.github.com>
2023-05-30 13:34:04 +0530

    Add files via upload

---
+++ importance.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const importance = () => {
+return (
+    <article id="main-content" class="col-lg-10 standard standard-layout layout-2Col leftnav-true">
+        <p>One of the fundamental tools for time management is that list of things you need to get done. It consolidates all your tasks in one place. From there you can prioritize them and tackle the important ones first.</p>
+        <p>There are 5 key advantages to maintaining a to-do list:</p>
+        <p><strong>A to-do list doesn’t forget</strong></p>
+        <p>Your brain is not the most efficient memory tool and will only trust systems that it knows works. Good memory recall is as simple as finding those things that will jog your brain at the time it needs to remember. Having a written list helps us remember when things have do be done so we do not miss anything.</p>
+        <p><strong>A to-do list helps you set priorities</strong></p>
+        <p>Making a to-do list is an important first step but prioritizing that list ensures that you focus on the most important items rather than giving in to the temptation of working on less important items because they may stand out more or because they are easier to do. Once you have a list of the things you need to complete, set priorities and decide which jobs should be done first.</p>
+        <p><strong>A to-do list lets you coordinate similar tasks</strong></p>
+        <p>A to-do list helps us to avoid repetition of labour. For example, if we have to deliver a document at an office and collect a document from another office which is on the same block, both these tasks can be done together. A load of time is lost in the starting, stopping and changing of different levels or types of activity. Save time by performing like tasks together. Make all your outgoing phone calls at the same time; organize your errands into a single run; reply to e-mail; etc. You will find this a more efficient use of your time.</p>
+        <p><strong>A to-do list tracks your progress</strong></p>
+        <p>Using a to-do list enables you to mark off the tasks you have completed. At the end of the day, when you look at the list, it will give you a sense of accomplishment and satisfaction. It might also have the effect of waking you up if nothing has been marked completed.</p>
+        <p><strong>A to-do list makes it easy to carry-over tasks</strong></p>
+        <p>If anything remains incomplete at the end of the day, it can be carried over to tomorrow’s list. This is an easy way of preparing a to-do list for the next day; by examining the to-do list of today and carrying forward any task that is incomplete.</p>
+    </article>
+)
+}
+
+export default importance
